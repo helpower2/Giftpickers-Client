58675f6e1e6ee1648fb623f8faf91fe281dd6b44
helpower2 <jeffreymonnink@gmail.com>
2020-10-21 00:08:12 +0200

    added the basic code

---
+++ Assets/Scripts/Multiplayer/ClientHandle.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using UnityEngine;
+
+public class ClientHandle : MonoBehaviour
+{
+    public static void Welcome(Packet _packet)
+    {
+        var _msg = _packet.ReadString();
+        var _myId = _packet.ReadInt();
+
+        Debug.Log($"Message from server: {_msg}");
+        Client.Instance.myId = _myId;
+        ClientSend.WelcomeReceived();
+
+        // Now that we have the client's id, connect UDP
+        Client.Instance.udp.Connect(((IPEndPoint) Client.Instance.tcp.socket.Client.LocalEndPoint).Port);
+    }
+
+    public static void SpawnPlayer(Packet _packet)
+    {
+        var _id = _packet.ReadInt();
+        var _username = _packet.ReadString();
+        var _position = _packet.ReadVector3();
+        var _rotation = _packet.ReadQuaternion();
+
+        GameManager.Instance.SpawnPlayer(_id, _username, _position, _rotation);
+    }
+
+    public static void PlayerPosition(Packet _packet)
+    {
+        var _id = _packet.ReadInt();
+        var _position = _packet.ReadVector3();
+
+        GameManager.players[_id].transform.position = _position;
+    }
+
+    public static void PlayerRotation(Packet _packet)
+    {
+        var _id = _packet.ReadInt();
+        var _rotation = _packet.ReadQuaternion();
+
+        GameManager.players[_id].transform.rotation = _rotation;
+    }
+}
---
+++ Assets/Scripts/Multiplayer/ClientSend.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class ClientSend : MonoBehaviour
+{
+    /// <summary>Sends a packet to the server via TCP.</summary>
+    /// <param name="_packet">The packet to send to the sever.</param>
+    private static void SendTCPData(Packet _packet)
+    {
+        _packet.WriteLength();
+        Client.Instance.tcp.SendData(_packet);
+    }
+
+    /// <summary>Sends a packet to the server via UDP.</summary>
+    /// <param name="_packet">The packet to send to the sever.</param>
+    private static void SendUDPData(Packet _packet)
+    {
+        _packet.WriteLength();
+        Client.Instance.udp.SendData(_packet);
+    }
+
+    #region Packets
+
+    /// <summary>Lets the server know that the welcome message was received.</summary>
+    public static void WelcomeReceived()
+    {
+        using (var _packet = new Packet((int) ClientPackets.welcomeReceived))
+        {
+            _packet.Write(Client.Instance.myId);
+            _packet.Write(UIManager.Instance.usernameField.text);
+
+            SendTCPData(_packet);
+        }
+    }
+
+    /// <summary>Sends player input to the server.</summary>
+    /// <param name="_inputs"></param>
+    public static void PlayerMovement(bool[] _inputs)
+    {
+        using (var _packet = new Packet((int) ClientPackets.playerMovement))
+        {
+            _packet.Write(_inputs.Length);
+            foreach (var _input in _inputs) _packet.Write(_input);
+            _packet.Write(GameManager.players[Client.Instance.myId].transform.rotation);
+
+            SendUDPData(_packet);
+        }
+    }
+
+    #endregion
+}
