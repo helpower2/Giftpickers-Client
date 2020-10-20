09bcc2c9dcc48d4923244a5415d5d171718a2ed0
helpower2 <jeffreymonnink@gmail.com>
2020-10-20 22:17:23 +0200

    added client shit

---
+++ Assets/Scripts/Multiplayer/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager>
+{
+    public GameObject startMenu;
+    public InputField usernameField;
+    
+
+    /// <summary>Attempts to connect to the server.</summary>
+    public void ConnectToServer()
+    {
+        startMenu.SetActive(false);
+        usernameField.interactable = false;
+        Client.Instance.ConnectToServer();
+    }
+}
